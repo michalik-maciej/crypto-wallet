94b0b766309a8820e7c5d3732ad56d4f38fbdb24
michalik-maciej <halniak24@gmail.com>
2022-02-27 23:30:15 +0100

    Add readme

--- src/components/views/HomePage/HomePage.tsx
+++ src/components/views/HomePage/HomePage.tsx
@@ -1,11 +1,5 @@
 import MainTable from '../../features/MainTable/MainTable'
-import CoinSearch from '../../features/CoinSearch/CoinSearch'
 
 export default function HomePage() {
-  return (
-    <>
-      <CoinSearch />
-      <MainTable />
-    </>
-  )
+  return <MainTable />
 }
